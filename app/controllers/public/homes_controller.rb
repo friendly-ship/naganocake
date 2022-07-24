f81deaa5620a28a97fb8601ac3cc77778c85c56e
EC2 Default User <ec2-user@ip-172-31-40-214.ap-northeast-1.compute.internal>
2022-07-24 15:06:34 +0000

    コントローラー記入

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,12 @@
 class Public::HomesController < ApplicationController
+
   def top
+    @genres = Genre.where(valid_invalid_status: 0)
+    @items = Item.limit(8).offset(4)
   end
 
   def about
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,41 @@
 Rails.application.routes.draw do
-  scope module: :public do
-    get 'items/index'
-    get 'items/show'
+ root 'public/homes#top'
+    devise_for :members, controllers: {
+    sessions:      'members/sessions',
+    passwords:     'members/passwords',
+    registrations: 'members/registrations'
+    }
+
+    devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+    }
+
+  namespace :admin do
+    get '/admins' => 'admins#top'
+    resources :items
+    resources :genres
+    resources :members
+    resources :orders,only:[:index,:show,:update]
+    resources :order_items, only:[:update]
   end
-  root to: 'public/homes#top'
-  get '/adout' => 'public/homes#about'
-  
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: 'public/registrations',
-    sessions: 'public/sessions'
-  }
-   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions:      'admin/sessions'
-  }
+
+  namespace :public do
+    get '/about' => 'homes#about'
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :items, only:[:index,:show,:new] do
+        get :search, on: :collection # ジャンル検索機能用
+    end
+    resources :cart_items
+    post '/orders/session' => 'orders#session_create'
+    get '/orders/confirm' => 'orders#confirm'
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/thanks' => 'orders#thanks'
+    patch '/members/withdrawal' => 'members#destroy'
+    get '/members/withdrawal' => 'members#withdrawal'
+    resources :orders, only:[:new,:create,:index,:show]
+    resource :members, only:[:show ,:edit,:update]
+    resources :addresses, only:[:index, :edit, :destroy, :create, :update]
+  end
 end
