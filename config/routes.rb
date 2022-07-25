4530036300518f30022fc8ba8a0f152b6560a26b
EC2 Default User <ec2-user@ip-172-31-59-207.us-west-2.compute.internal>
2022-07-25 05:51:59 +0000

    nagano

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/complete'
+    get 'orders/finish'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
  devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
