e70b27b56e253aa17a183f26019c3beefcb27cb3
EC2 Default User <ec2-user@ip-172-31-47-151.ec2.internal>
2022-07-25 06:26:43 +0000

    ジャンル

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_07_24_060712) do
+ActiveRecord::Schema.define(version: 2022_07_25_044106) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -43,16 +42,8 @@ ActiveRecord::Schema.define(version: 2022_07_24_060712) do
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
     t.string "email", default: "", null: false
-    t.string "adress"
-    t.string "postal_code"
     t.string "encrypted_password", default: "", null: false
-    t.string "telephone_number"
-    t.string "is_deleted"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -60,6 +51,12 @@ ActiveRecord::Schema.define(version: 2022_07_24_060712) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "items", force: :cascade do |t|
